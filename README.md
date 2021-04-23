064ad4b799211d1d15df733979478862e0e58252
frrfdev <feliperenanrf23@gmail.com>
2021-04-22 23:18:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Prefix: !e
+
+Commands: 
+    - disable
+    - enable
---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+const prefix = "!e";
+const emojiRegex = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+var enabled = true;
+
+client.on("message", function(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+        
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if(!command) return;
+    
+    enabled = command === "disable" ? false : command === "enabled" ? true : enabled
+});
+
+client.on("message", function(message) {
+    if(!enabled) return;
+
+    const match = emojiRegex.exec(message.content);
+    if (!match) return;
+    message.delete();
+})
+
+client.login(config.BOT_TOKEN);
